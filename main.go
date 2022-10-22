38fdbc9415bee5fedd17516c3fd45894692424ad
Masanori Tani <metro@uta8a.org>
2022-10-22 11:52:51 +0000

    feat: サーバとして利用するための機能を追加

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
 )
 
 type City struct {
@@ -17,21 +19,40 @@ type City struct {
 	Population  int    `json:"population,omitempty" db:"Population"`
 }
 
+var (
+	db *sqlx.DB
+)
+
 func main() {
 	// if len(os.Args) < 2 {
 	// 	log.Fatalln("usage: go run main.go <city_name>")
 	// }
 	// cityName := os.Args[1]
 
-	db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
+	_db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
 	if err != nil {
 		log.Fatalf("Cannot Connect to Database: %s", err)
 	}
 
-	fmt.Println("Connected!")
-	// city := City{}
-	// db.Get(&city, "SELECT * FROM city WHERE Name = ?", cityName)
+	db = _db
+	e := echo.New()
 
-	// fmt.Printf("%sの人口は%d人です\n", cityName, city.Population)
-	db.Exec("INSERT INTO city (Name, CountryCode, District, Population) VALUES ('wakuwaku-ookoku', 'JPN', 'wakuwaku-island', 100)")
+	e.GET("/cities/:cityName", getCityInfoHandler)
+
+	e.Start(":4000")
 }
+
+func getCityInfoHandler(c echo.Context) error {
+	cityName := c.Param("cityName")
+	fmt.Println(cityName)
+
+	city := City{}
+	db.Get(&city, "SELECT * FROM city WHERE Name=?", cityName)
+	if city.Name == "" {
+		return c.NoContent(http.StatusNotFound)
+	}
+	return c.JSON(http.StatusOK, city)
+}
+
+// $ curl localhost:4000/cities/Osaka
+// {"id":1534,"name":"Osaka","countryCode":"JPN","district":"Osaka","population":2595674}
