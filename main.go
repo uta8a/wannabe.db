b1c445be4a6bd08b379e74ab39bd765d41634cb6
Masanori Tani <metro@uta8a.org>
2022-10-22 12:25:17 +0000

    feat: 都市を追加するAPIを追加

--- main.go
+++ main.go
@@ -19,16 +19,18 @@ type City struct {
 	Population  int    `json:"population,omitempty" db:"Population"`
 }
 
+type RequestedCity struct {
+	Name        string `json:"name,omitempty" db:"Name"`
+	CountryCode string `json:"countryCode,omitempty" db:"CountryCode"`
+	District    string `json:"district,omitempty" db:"District"`
+	Population  int    `json:"population,omitempty" db:"Population"`
+}
+
 var (
 	db *sqlx.DB
 )
 
 func main() {
-	// if len(os.Args) < 2 {
-	// 	log.Fatalln("usage: go run main.go <city_name>")
-	// }
-	// cityName := os.Args[1]
-
 	_db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
 	if err != nil {
 		log.Fatalf("Cannot Connect to Database: %s", err)
@@ -38,6 +40,7 @@ func main() {
 	e := echo.New()
 
 	e.GET("/cities/:cityName", getCityInfoHandler)
+	e.POST("/cities", addCityHandler)
 
 	e.Start(":4000")
 }
@@ -54,5 +57,24 @@ func getCityInfoHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, city)
 }
 
+func addCityHandler(c echo.Context) error {
+	var cityData RequestedCity
+	if err := c.Bind(&cityData); err != nil {
+		return c.JSON(http.StatusBadRequest, cityData)
+	}
+	fmt.Println(cityData)
+	if _, err := db.NamedExec("INSERT INTO city (Name, CountryCode, District, Population) VALUES (:Name,:CountryCode,:District,:Population)", &cityData); err != nil {
+		fmt.Println(err)
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	return c.JSON(http.StatusOK, cityData)
+}
+
 // $ curl localhost:4000/cities/Osaka
 // {"id":1534,"name":"Osaka","countryCode":"JPN","district":"Osaka","population":2595674}
+
+// $ curl -X POST -H "Content-Type: application/json" -d '{"name":"NekoNekoClub","countryCode":"JPN","district":"Osaka","population":100}' localhost:4000/cities
+// {"name":"NekoNekoClub","countryCode":"JPN","district":"Osaka","population":100}
+
+// $ curl localhost:4000/cities/NekoNekoClub
+// {"id":4081,"name":"NekoNekoClub","countryCode":"JPN","district":"Osaka","population":100}
