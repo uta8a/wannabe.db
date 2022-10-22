5edd188c5d43f6167bf0ba455d17579bc756b9cd
Masanori Tani <metro@uta8a.org>
2022-10-22 11:28:57 +0000

    feat: 街の名前を入れると人口を返すプログラム

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+type City struct {
+	ID          int    `json:"id,omitempty" db:"ID"`
+	Name        string `json:"name,omitempty" db:"Name"`
+	CountryCode string `json:"countryCode,omitempty" db:"CountryCode"`
+	District    string `json:"district,omitempty" db:"District"`
+	Population  int    `json:"population,omitempty" db:"Population"`
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalln("usage: go run main.go <city_name>")
+	}
+	cityName := os.Args[1]
+
+	db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
+	if err != nil {
+		log.Fatalf("Cannot Connect to Database: %s", err)
+	}
+
+	fmt.Println("Connected!")
+	city := City{}
+	db.Get(&city, fmt.Sprintf("SELECT * FROM city WHERE Name = '%s'", cityName))
+
+	fmt.Printf("%sの人口は%d人です\n", cityName, city.Population)
+}
