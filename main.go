2f574ca98204554bc6ece12000e0b9ebe7386125
Masanori Tani <metro@uta8a.org>
2022-10-22 11:41:31 +0000

    feat: データを挿入するコードを追加(実行後手動でデータを削除)

--- main.go
+++ main.go
@@ -18,10 +18,10 @@ type City struct {
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatalln("usage: go run main.go <city_name>")
-	}
-	cityName := os.Args[1]
+	// if len(os.Args) < 2 {
+	// 	log.Fatalln("usage: go run main.go <city_name>")
+	// }
+	// cityName := os.Args[1]
 
 	db, err := sqlx.Connect("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOSTNAME"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
 	if err != nil {
@@ -29,9 +29,9 @@ func main() {
 	}
 
 	fmt.Println("Connected!")
-	city := City{}
-	db.Get(&city, "SELECT * FROM city WHERE Name = ?", cityName)
+	// city := City{}
+	// db.Get(&city, "SELECT * FROM city WHERE Name = ?", cityName)
 
-	fmt.Printf("%sの人口は%d人です\n", cityName, city.Population)
-
+	// fmt.Printf("%sの人口は%d人です\n", cityName, city.Population)
+	db.Exec("INSERT INTO city (Name, CountryCode, District, Population) VALUES ('wakuwaku-ookoku', 'JPN', 'wakuwaku-island', 100)")
 }
